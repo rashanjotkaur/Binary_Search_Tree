b2c09f3dffb972d81994101c041c11038ae45908
rashan <40189953+rashanjotkaur@users.noreply.github.com>
2020-07-26 03:00:50 -0700

    Update morris_traversal.cpp

--- morris_traversal.cpp
+++ morris_traversal.cpp
@@ -1,5 +1,5 @@
 void MorrisTraversal(struct Node* root){ 
-    struct tNode *curr,*pre;
+    struct Node *curr;
     if (root == NULL) 
         return; 
   
@@ -11,18 +11,18 @@ void MorrisTraversal(struct Node* root){
         } 
         else { 
             /* Find the inorder predecessor of current */
-            pre = curr->left; 
+            struct Node *pre = curr->left; 
             while (pre->right != NULL && pre->right != curr) 
                 pre = pre->right; 
   
             /* Make current as the right child of its inorder predecessor */
-            if (pre->right == NULL) { 
+            if(pre->right == NULL){ 
                 pre->right = curr; 
                 curr = curr->left; 
             } 
   
             /* Revert the changes made in the 'if' part to restore the original tree i.e., fix the right child of predecessor */
-            else { 
+            else{ 
                 pre->right = NULL; 
                 cout<<curr->data<<endl; 
                 curr = curr->right; 

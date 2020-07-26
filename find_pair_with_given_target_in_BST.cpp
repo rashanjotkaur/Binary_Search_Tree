86a519dee4b2384cb46c7e7fa7e31bc628eefc8d
rashan <40189953+rashanjotkaur@users.noreply.github.com>
2020-07-26 05:12:13 -0700

    Create find_pair_with_given_target_in_BST.cpp

---
+++ find_pair_with_given_target_in_BST.cpp
@@ -0,0 +1,26 @@
+// Method 1-Using O(n) auxillary space
+void inorder(struct Node *root,vector <int> &v){
+    if(root==NULL)
+        return ;
+    inorder(root->left,v);
+    v.push_back(root->data);
+    inorder(root->right,v);
+}
+bool isPairPresent(struct Node *root, int sum){
+    if(root==NULL)
+        return false;
+    vector <int> v;
+    inorder(root,v);
+    
+    int i=0,j=v.size()-1;
+    while(i<j){
+        int x=v[i]+v[j];
+        if(x==sum)
+            return true;
+        if(x<sum)
+            i++;
+        else
+            j--;
+    }
+    return false;
+}

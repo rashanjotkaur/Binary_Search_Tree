a59b8b40f074321498b4a001531c40dc4c3e6e8a
rashan <40189953+rashanjotkaur@users.noreply.github.com>
2020-07-27 02:40:53 -0700

    Update fixed_two_nodes_of_BST.cpp

--- fixed_two_nodes_of_BST.cpp
+++ fixed_two_nodes_of_BST.cpp
@@ -1,4 +1,31 @@
 // https://practice.geeksforgeeks.org/problems/fixed-two-nodes-of-a-bst/1/
+// Method 1-O(n) space;
+void traverse(struct Node *root,vector <int> &v){
+    if(root==NULL)
+        return ;
+    traverse(root->left,v);
+    v.push_back(root->data);
+    traverse(root->right,v);
+}
+void inorder(struct Node *&root,vector <int> v,int &i){
+    if(root==NULL)
+        return ;
+    inorder(root->left,v,i);
+    root->data=v[i];
+    i++;
+    inorder(root->right,v,i);
+}
+struct Node *correctBST(struct Node* root){
+    if(root==NULL)
+        return root;
+    vector <int> v;
+    traverse(root,v);
+    sort(v.begin(),v.end());
+    int i=0;
+    inorder(root,v,i);
+    return root;
+}
+
 // Method 2-O(1) space;
 void inorder(Node *root,Node *&first,Node *&mid,Node *&last,
     Node *&prev){

9fc4a225a0597fb78f1afbb049d712102cc0acc8
rashan <40189953+rashanjotkaur@users.noreply.github.com>
2020-07-29 21:22:13 -0700

    Update merge_two_BSTs.cpp

--- merge_two_BSTs.cpp
+++ merge_two_BSTs.cpp
@@ -1,3 +1,4 @@
+// Method 1-Store inorder traversal of both trees and then use sorting
 void inorder(struct Node *root,vector <int> &v){
     if(root==NULL)
         return ;
@@ -41,4 +42,42 @@ vector<int> merge(Node *root1, Node *root2){
         j++;
     }
     return v;
+}
+
+// Method 2- using inorder with Stack
+// Time Complexity-O(n+m)
+// Space Complexity-O(height of T1 + height of T2)
+void insertNodes(Node *root, stack<Node*> &s){
+    while(root!=NULL){
+        s.push(root);
+        root=root->left;
+    }
+}
+vector<int> merge(Node *root1, Node *root2){
+    vector <int> v;
+    stack <Node*> s1;
+    stack <Node*> s2;
+ 
+    insertNodes(root1,s1);
+    insertNodes(root2,s2);
+   
+    while(!s1.empty() || !s2.empty()){
+        int a=(!s1.empty() ? s1.top()->data:INT_MAX);
+        int b=(!s2.empty() ? s2.top()->data:INT_MAX);
+       
+       if(a<=b){
+           v.push_back(a);
+           Node *temp=s1.top();
+           s1.pop();
+           insertNodes(temp->right,s1);
+       }
+       else{
+           v.push_back(b);
+           Node *temp=s2.top();
+           s2.pop();
+           insertNodes(temp->right,s2);
+           
+       }
+   }
+   return v;
 }
